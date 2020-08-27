22f69ec67b8b2b94e64758f3a5c07526c646693b
Jean-Yves BAUDY <baudy.jy@fr.ibm.com>
2020-08-27 09:21:58 +0200

    Add ZUNIT

--- cics-genapp/base/src/COBOL/LGICDB01.cbl
+++ cics-genapp/base/src/COBOL/LGICDB01.cbl
@@ -16,7 +16,7 @@
       *                    Inquire Customer                            *
       *                                                                *
       * Select customer details from DB2 table                         *
-      * T10                                                            *
+      * T11                                                            *
       *                                                                *
       ******************************************************************
        IDENTIFICATION DIVISION.
