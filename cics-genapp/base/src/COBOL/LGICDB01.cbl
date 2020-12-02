7e4be64febb1eb15ddf45ec080c6a348a7e6178a
Joseph Taffe-Atkins <Jt-atkins@uk.ibm.com>
2020-12-02 10:08:22 +0000

    Update LGICDB01.cbl

--- cics-genapp/base/src/COBOL/LGICDB01.cbl
+++ cics-genapp/base/src/COBOL/LGICDB01.cbl
@@ -6,7 +6,7 @@
       *                                                                *
       * CB12.                                                          *
       *                                                                *
-      * (C) COPYRIGHT IBM CORP. 2011, 2013 ALL RIGHTS RESERVED         *
+      * (C) COPYRIGHT IBM CORP. 2011,2013 ALL RIGHTS RESERVED          *
       *                                                                *
       * US GOVERNMENT USERS RESTRICTED RIGHTS - USE, DUPLICATION,      *
       * OR DISCLOSURE RESTRICTED BY GSA ADP SCHEDULE                   *
