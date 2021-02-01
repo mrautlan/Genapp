c4b120e8a8dc70fef4508e8ca4e73423b8d93473
Joseph Taffe-Atkins <Jt-atkins@uk.ibm.com>
2021-02-01 12:48:34 +0000

    Update TLGICDB0.cbl

--- cics-genapp/zUnit/testcase/TLGICDB0.cbl
+++ cics-genapp/zUnit/testcase/TLGICDB0.cbl
@@ -2,7 +2,7 @@
       *+---------------------------------------------------------------+
       *| TLGICDB0                                                      |
       *| PRODUCT: IBM DEVELOPER FOR Z/OS                               |
-      *| COMPONENT: IBM Z/OS AUTOMATED UNIT TESTING FRAMEWORK (ZUNIT)  |
+      *| COMPONENT: IBM Z/OS AUTOMATED UNIT TESTINGS RAMEWORK (ZUNIT)  |
       *|   FOR ENTERPRISE COBOL AND PL/I                               |
       *| PROGRAM: ENTERPRISE COBOL ZUNIT TEST CASE FOR DYNAMIC RUNNER  |
       *| DATE GENERATED: 01/31/2021 01:31                              |
