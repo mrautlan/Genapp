401767dd279edf74d07e190a953bf472400cd89c
prince111j <jt-atkins@uk.ibm.com>
2021-08-18 19:27:13 +0000

    ZPROJ-123 test test

--- cics-genapp/base/src/COBOL/LGICDB01.cbl
+++ cics-genapp/base/src/COBOL/LGICDB01.cbl
@@ -17,7 +17,7 @@
       *                                                                *
       * Select customer details from DB2 table                         *
       *                                                                *
-      * TEST: 01                                                       *
+      * TEST: 011-                                                     *
       *                                                                *
       ******************************************************************
        IDENTIFICATION DIVISION.
