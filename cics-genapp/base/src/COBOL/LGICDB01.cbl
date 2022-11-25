89ff8294d4ae14694631081c4a072fc5cd08c6f9
Joseph Taffe-Atkins <Jt-atkins@uk.ibm.com>
2022-11-25 15:53:36 +0000

    Test for Jagan

--- cics-genapp/base/src/COBOL/LGICDB01.cbl
+++ cics-genapp/base/src/COBOL/LGICDB01.cbl
@@ -2,7 +2,7 @@
       *                                                                *
       * LICENSED MATERIALS - PROPERTY OF IBM                           *
       *                                                                *
-      * "RESTRICTED MATERIALS OF IBMl                                  *
+      * "RESTRICTED MATERIALS OF IBM.                                  *
       *                                                                *
       * CB12                                                           *
       *                                                                *
